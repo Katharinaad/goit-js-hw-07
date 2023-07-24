19be8be92609f710e6902b4a948fd61f2f12d27f
Katharinaad <vermilion.adams@gmail.com>
2023-07-24 20:01:04 +0200

    Create 02-lightbox.js

---
+++ js/02-lightbox.js
@@ -0,0 +1,34 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const ulGallery = document.querySelector(".gallery");
+const markup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+ulGallery.innerHTML = markup;
+
+ulGallery.addEventListener("click", imageClick);
+
+function imageClick(event) {
+  event.preventDefault();
+
+  let galleryLightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 200,
+  });
+}
